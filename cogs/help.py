6e7ae42d245a848d3fc7ceab51c58b303f6f4d29
Ergastolator1 <53176531+Ergastolator1@users.noreply.github.com>
2020-08-04 15:17:19 +0200

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -33,6 +33,21 @@ class HelpCommand(commands.Cog):
 			**{ctx.prefix}stop** - Stops playing song and clears the queue.
 			**{ctx.prefix}summon** - Summons the bot to a voice channel.
 		""", inline=True)
+		.add_field(name="Fun", value=f"""
+			**{ctx.prefix}choose** - Choose between multiple options.
+			**{ctx.prefix}roll** - Roll a random number.
+			**{ctx.prefix}flip** - Flip a coin.
+			**{ctx.prefix}rps** - Play Rock, Paper, Scissors.
+			**{ctx.prefix}8ball** - Ask 8 ball a question.
+			**{ctx.prefix}lmgtfy** - Create a lmgtfy link.
+			**{ctx.prefix}say** - Make the bot say something.
+			**{ctx.prefix}reverse** - !txet ruoy esreveR
+			**{ctx.prefix}meme** - Get a random meme.
+			**{ctx.prefix}emojify** - Turns your text into emojis!
+			**{ctx.prefix}roast** - Roast someone! If you suck at roasting them yourself.
+			**{ctx.prefix}smallcaps** - ᴄᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ᴛᴇxᴛ ᴛᴏ ꜱᴍᴀʟʟ ᴄᴀᴘꜱ!!
+			**{ctx.prefix}cringe** - mAkE ThE TeXt cRiNgY!!
+		""", inline=False)
 		.set_footer(text="Made by Ergastolator#0001")
 		.set_author(name="StrapBot",icon_url="https://cdn.discordapp.com/avatars/740140581174378527/226deca56aaa9cbe5f27dcbf7dda732d.png?size=64")
 		.set_thumbnail(url="https://cdn.discordapp.com/avatars/740140581174378527/226deca56aaa9cbe5f27dcbf7dda732d.png?size=256"))
