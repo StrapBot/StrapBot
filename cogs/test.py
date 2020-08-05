99af4f746597e5997a10ec1e903010993f330feb
Ergastolator1 <53176531+Ergastolator1@users.noreply.github.com>
2020-08-05 15:33:48 +0200

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -41,6 +41,18 @@ class Test(commands.Cog):
 		.set_thumbnail(url="https://cdn.discordapp.com/avatars/740140581174378527/226deca56aaa9cbe5f27dcbf7dda732d.png?size=256")
 		.set_author(name="StrapBot",icon_url="https://cdn.discordapp.com/avatars/740140581174378527/226deca56aaa9cbe5f27dcbf7dda732d.png?size=64"))
 
+	@commands.command(pass_context=True)
+	async def invite(self, ctx):
+		"""Invite me on your servers!"""
+		return await ctx.send(embed=discord.Embed(
+			title="Invite me!",
+			description=f"If you want to invite me, [click here](https://bit.ly/StrapBot)!",
+			color=discord.Color.lighter_grey()
+		)
+		.set_footer(text="Made by Ergastolator#0001")
+		.set_thumbnail(url="https://cdn.discordapp.com/avatars/740140581174378527/226deca56aaa9cbe5f27dcbf7dda732d.png?size=256")
+		.set_author(name="StrapBot",icon_url="https://cdn.discordapp.com/avatars/740140581174378527/226deca56aaa9cbe5f27dcbf7dda732d.png?size=64"))
+
 
 def setup(bot):
 	bot.add_cog(Test(bot))
