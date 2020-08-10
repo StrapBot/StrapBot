4911d0acab2c15c3a8f074851cea431ef2db440e
Ergastolator1 <53176531+Ergastolator1@users.noreply.github.com>
2020-08-10 10:13:49 +0200

    Update strapbot.py

--- strapbot.py
+++ strapbot.py
@@ -18,12 +18,19 @@ if __name__ == "__main__":
 		except (discord.ClientException, ModuleNotFoundError):
 			if extension == ".DS_Store":
 				pass
+			elif extension == ".gitignore":
+				pass
 			else:
 				print(f"Failed to load extension {extension}.")
+				print(traceback_format_exc())
 
 @bot.event
 async def on_ready():
-	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{bot.command_prefix}help for help."),status=discord.Status.online)
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} servers! | Use {bot.command_prefix}help for help."),status=discord.Status.online)
 	print('StrapBot is logged in as {0.user}!'.format(bot))
+
+@bot.event
+async def on_guild_leave(guild):
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} servers! | Use {bot.command_prefix}help for help."),status=discord.Status.online)
 
 bot.run(token)
