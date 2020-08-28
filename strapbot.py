fddb411a05bf0d5ba8b8720b17d6382bdd2cb84f
Vincysuper07 <vincysuper07@gmail.com>
2020-08-28 13:09:34 +0200

    starting 2 langs support

--- strapbot.py
+++ strapbot.py
@@ -1,12 +1,16 @@
 import os
 from os.path import isfile, join
+import pymongo
 import discord
 import asyncio
 from discord.ext import commands
 from dotenv import load_dotenv as import_dotenv
+from core.languages import default_language
 
 import_dotenv()
 token = os.getenv("TOKEN")
+
+db = pymongo.MongoClient(os.getenv("MONGO_URI"))
 
 bot = commands.Bot(command_prefix="sb.")
 bot.remove_command("help")
@@ -27,14 +31,24 @@ if __name__ == "__main__":
 @bot.event
 async def on_ready():
 	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} servers! | Use {bot.command_prefix}help for help."),status=discord.Status.online)
-	print('StrapBot is logged in as {0.user}!'.format(bot))
+	if default_language == "en":
+		print('StrapBot is logged in as {0.user}!'.format(bot))
+	elif default_language == "it":
+		print("StrapBot loggato come {0.user}!".format(bot))
 
 @bot.event
 async def on_guild_join(guild):
-	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} servers! | Use {bot.command_prefix}help for help."),status=discord.Status.online)
+	if default_language == "en":
+		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} servers! | Use {bot.command_prefix}help for help."),status=discord.Status.online)
+	elif default_language == "it":
+		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} server! | Usa {bot.command_prefix}help per i comandi."),status=discord.Status.online)
+
 
 @bot.event
 async def on_guild_remove(guild):
-	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} servers! | Use {bot.command_prefix}help for help."),status=discord.Status.online)
+	if default_language == "en":
+		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} servers! | Use {bot.command_prefix}help for help."),status=discord.Status.online)
+	elif default_language == "it":
+		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f"{len(bot.guilds)} server! | Usa {bot.command_prefix}help per i comandi."),status=discord.Status.online)
 
 bot.run(token)
