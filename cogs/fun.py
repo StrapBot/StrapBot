66f6b153d74b7e1180ffe6d6ca38c7902d2c6028
Ergastolator1 <53176531+Ergastolator1@users.noreply.github.com>
2020-09-12 12:09:50 +0200

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -158,9 +158,12 @@ class Fun(commands.Cog):
         for guild in self.bot.guilds:
             if guild.id == 725719209228763136:
                 await ctx.send("this command is blacklisted on this server due to abuse, I'm sorry.")
+                break
             else:
                 msg = escape(message,mass_mentions=True)
                 await ctx.send(msg)
+                break
+
     @commands.command()
     async def reverse(self, ctx, *, text):
         """!txeT ruoY esreveR"""
