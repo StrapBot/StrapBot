09ab6b933242a23fa267e114bcae7afe6b183137
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-09-27 09:06:50 +0200

    Update languages.py

--- core/languages.py
+++ core/languages.py
@@ -1,10 +1,6 @@
 import os
 import discord
 
-default_language = os.getenv("DEFAULT_LANGUAGE")
-
-if default_language == None: #defaults to english
-    default_language = "en"
-
+default_language = os.getenv("DEFAULT_LANGUAGE", "en")
 
 #raise NotImplementedError
