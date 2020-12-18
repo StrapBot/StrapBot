cd9c42c063f0742c2c8f5ca6bbf1dc8ca5b096a2
vincy <vincysuper07@gmail.com>
2020-12-18 18:42:15 +0100

    finally, it works

--- cogs/utils.py
+++ cogs/utils.py
@@ -8,6 +8,7 @@ class Utilities(commands.Cog):
         self.bot = bot
         self._original_help_command = bot.help_command
         bot.help_command = HelpCommand(
+            verify_checks=False,
             command_attrs={
                 "name": "help",
                 "aliases": ["man", "h"],
@@ -15,8 +16,8 @@ class Utilities(commands.Cog):
         )
         bot.help_command.cog = self
 
-        def cog_unload(self):
-            self.bot.help_command = self._original_help_command
+    def cog_unload(self):
+        self.bot.help_command = self._original_help_command
 
     @commands.Cog.listener()
     async def on_ready(self):
